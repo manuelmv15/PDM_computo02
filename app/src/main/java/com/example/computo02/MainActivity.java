d08cb9e8dce94792c5f2b8fd7735264b3c0de570
mv-mnl <mv23036@ues.edu.sv>
2026-04-15 22:34:20 +0000

    clase_15_04

--- app/src/main/java/com/example/computo02/MainActivity.java
+++ app/src/main/java/com/example/computo02/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.computo02;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.ContentInfoCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView tvMostrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,21 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
+
+
+
         });
+
+
+        tvMostrar = findViewById(R.id.tvMostrar);
+
+        SharedPreferences archivo = getSharedPreferences("data_tem", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit  = archivo.edit();
+        edit.putString("TOKEN", "1234");
+        edit.apply();
+
+        tvMostrar.setText(archivo.getString("TOKEN", "No encontrado"));
     }
 }
